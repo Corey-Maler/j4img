ed223ec8b0ea12c8bfdf805e68269424723927f9
Corey Maler <corey.maler@outlook.com>
2016-03-26 20:30:35 +0300

    react and webpack boilerplate

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {render} from 'react-dom';
+import J4img from './j4img';
+
+render(
+  <J4img />,
+  document.getElementById('root')
+);
---
+++ scripts/j4img.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+export default class J4img extends Component {
+  render() {
+    return (
+      // Add your component markup and other subcomponent references here.
+      <h1>j4img, bitch!</h1>
+    );
+  }
+}
